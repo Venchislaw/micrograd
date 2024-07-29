0d206c6428b53b90cdb2249cd7e2c88e327c71f2
Venchislaw <venchislavcodes@gmail.com>
2024-07-29 18:21:39 +0300

    Hell Yeah Simulator!

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -2,15 +2,15 @@ import numpy as np
 
 
 class Tensor:
-    def __init__(self, arr=[], prev=(), op="", fill_random=False, shape=None):
-        if len(arr) > 0:
-            self.arr = np.asarray(arr)
-        elif fill_random:
+    def __init__(self, arr=[], prev=(), op="", fill_random=False, fill_zeros=False, shape=None):
+        if fill_random:
             assert shape is not None, "Must pass Shape with random fill"
             self.arr = np.random.random(shape)
-        else:
+        elif fill_zeros:
             assert shape is not None, "Must pass shape with zeros fill"
-            self.arr = np.zeros(shape)
+            self.arr = np.zeros(shape, dtype=float)
+        else:
+            self.arr = np.asarray(arr, dtype=float)
 
         self.prev = prev
         self.op = op
@@ -36,14 +36,12 @@ class Tensor:
         other = other if isinstance(other, Tensor) else Tensor(other)
         out = Tensor(self.arr * other.arr, prev=(self, other), op="*")
 
-        self.check_broadcast(other)
-
         def grad_func():
             # Note!!! It's not Tensor.dot, it's np.dot
             self.grad += out.grad * other.arr
             other.grad += out.grad * self.arr
         
-        out.grad_func = grad_func()
+        out.grad_func = grad_func
         return out
     
     def dot(self, other):
@@ -51,17 +49,71 @@ class Tensor:
         assert self.shape[-1] == other.shape[0]
         out = Tensor(self.arr @ other.arr, prev=(self, other), op="dot")
 
-        self.check_broadcast(other)
-
         def grad_func():
             # Note!!! It's not Tensor.dot, it's np.dot
             self.grad += out.grad.dot(other.arr.T)
             other.grad += self.arr.T.dot(out.grad)
 
-        out.grad_fun = grad_func()
+        out.grad_fun = grad_func
+        return out
+    
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "Support only int or float"
+
+        out = Tensor(self.arr**other, prev=(self, ), op="**{other}")
+
+        def grad_func():
+            self.grad += out.grad * (other * self.arr ** (other - 1))
+            
+        out.grad_func = grad_func
         return out
     
 
+    def relu(self):
+        out = Tensor(np.maximum(self.arr, 0), prev=(self,), op="ReLU")
+
+        def grad_func():
+            self.grad += out.grad * (self.arr > 0)
+        
+        out.grad_func = grad_func
+        return out
+    
+    def tanh(self):
+        t = (np.exp(2 * self.arr) - 1) / (2 * np.exp(-self.arr))
+        out = Tensor(t, prev=(self, ), op="tanh")
+
+        def grad_func():
+            self.grad += out.grad * (1 - t**2)
+        
+        out.grad_func = grad_func
+        return out
+    
+    def sigmoid(self):
+        out = Tensor(1 / 1 + np.exp(-self.arr), prev=(self, ), op="sigmoid")
+
+        def grad_func():
+            self.grad += out.grad * (out.arr * (1 - out.arr))
+
+        out.grad_func = grad_func
+        return out 
+
+    def __neg__(self):
+        return Tensor(-self.arr)
+
+    def __sub__(self, other):
+        return self + -other
+    
+    def __truediv__(self, other):
+        return self * (other**-1)
+
+
+    def __rmul__(self, other):
+        return self * other
+    
+    def __radd__(self, other):
+        return self + other
+    
+    
     def check_broadcast(self, other):
         for n, (i, j) in enumerate(zip(self.shape, other.shape)):
             if i == j:
@@ -84,6 +136,8 @@ class Tensor:
     def __repr__(self):
         return f"Tensor object: {self.arr.shape}"
     
+    __array_priority__ = 10000
+    
 """
 a = Tensor([[1, 2, 3]])
 b = Tensor([[2, 3, 4]])
@@ -98,13 +152,22 @@ print(h)"""
 
 X = Tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
 W = Tensor(fill_random=True, shape=(10, 3))
-b = Tensor(shape=(10, 1))
+b = Tensor(fill_zeros=True, shape=(10, 1))
 
 Xt = X.T
 mul = W.dot(Xt)
-res = mul + b
+res_z = mul + b
+res_a = res_z.tanh()
 
-res.grad = np.ones(res.shape)
-res.grad_func()
+res_a.grad = np.ones(res_a.shape)
+res_a.grad_func()
+res_z.grad_func()
 mul.grad_func()
 print(W.grad)
+
+a = Tensor([1, 2, 3])
+
+z = a / Tensor([1, 2, 3])
+z.grad_func()
+
+print(z.grad)
