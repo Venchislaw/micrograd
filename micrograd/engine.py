1d8a8e5530c83ae7f863ac14b9282e1ee80bcc03
Venchislaw <venchislavcodes@gmail.com>
2024-07-29 16:56:08 +0300

    Retrograd starts to breathe

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -2,71 +2,101 @@ import numpy as np
 
 
 class Tensor:
-    def __init__(self, seq=[], prev=(), random_init=False, shape=None, op=""):
-        if not random_init and not shape:
-            self.seq = np.array(seq)
-        elif not random_init and shape:
-            self.seq = np.zeros(shape)
+    def __init__(self, arr=[], prev=(), op="", fill_random=False, shape=None):
+        if len(arr) > 0:
+            self.arr = np.asarray(arr)
+        elif fill_random:
+            assert shape is not None, "Must pass Shape with random fill"
+            self.arr = np.random.random(shape)
         else:
-            self.seq = np.random.random(shape)
+            assert shape is not None, "Must pass shape with zeros fill"
+            self.arr = np.zeros(shape)
 
-        self.prev = set(prev)
+        self.prev = prev
         self.op = op
-        self.grad = np.zeros(self.seq.shape)
-        self._backward = lambda: None
+        self.grad = np.zeros(self.arr.shape)
+        self.broadcast_dim = None
+
+        self.grad_func = lambda: None
 
     def __add__(self, other):
-        other if isinstance(other, Tensor) else Tensor(other)
-        out = Tensor(self.seq + other.seq, prev=(self, other), op="+")
-        out.grad = np.ones(out.shape())
-        def _backward():
-            self.grad += out.grad
-            other.grad += out.grad
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        out = Tensor(self.arr + other.arr, prev=(self, other), op="+")
 
-        out._backward = _backward
+        self.check_broadcast(other)
+
+        def grad_func():
+            self.grad += out.grad if not self.broadcast_dim else out.grad.sum(axis=self.broadcast_dim, keepdims=True)
+            other.grad += out.grad if not other.broadcast_dim else out.grad.sum(axis=other.broadcast_dim, keepdims=True)
+
+        out.grad_func = grad_func
         return out
     
     def __mul__(self, other):
-        other if isinstance(other, Tensor) else Tensor(other)
-        out = Tensor(self.seq * other.seq, prev=(self, other), op="*")
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        out = Tensor(self.arr * other.arr, prev=(self, other), op="*")
+
+        self.check_broadcast(other)
+
+        def grad_func():
+            # Note!!! It's not Tensor.dot, it's np.dot
+            self.grad += out.grad.dot(other.arr.T)
+            other.grad += self.grad.dot(self.arr.T)
+        
+        out.grad_func = grad_func()
         return out
     
-    def matmul(self, other):
-        other if isinstance(other, Tensor) else Tensor(other)
-        out = Tensor(np.dot(self.seq, other.seq), prev=(self, other), op="@")
-        return out
+    def dot(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        assert self.shape[-1] == other.shape[0]
+        out = np.dot(self.arr, other.arr)
+
+        return Tensor(out, prev=(self, other), op="dot")
     
-    def __neg__(self):
-        out = Tensor(-self.seq)
-        return out
-    
-    def __sub__(self, other):
-        out = self + -other
-        return out
-    
-    def T(self):
-        out = Tensor(self.seq.T, prev=(self,), op="T")
-        return out
-    
+
+    def check_broadcast(self, other):
+        for n, (i, j) in enumerate(zip(self.shape, other.shape)):
+            if i == j:
+                continue
+
+            elif i > j:
+                other.broadcast_dim = n
+            else:
+                self.check_broadcast = n
+
+
+    @property
     def shape(self):
-        out = self.seq.shape
-        return out
+        return self.arr.shape
+    
+    @property
+    def T(self):
+        return Tensor(self.arr.T, prev=(self,), op="T")
     
     def __repr__(self):
-        return f"Tensor object: {self.seq}"
+        return f"Tensor object: {self.arr.shape}"
+    
+"""
+a = Tensor([[1, 2, 3]])
+b = Tensor([[2, 3, 4]])
+c = a + b
+d = Tensor([[1, 2, 3]])
+e = c * d
+f = Tensor([[1, 2, 3]])
+g = f.T
+h = e.dot(g)
+print(h)"""
 
-X = Tensor([[1, 2], [3, 4], [5, 6]])
-w = Tensor(random_init=True, shape=(16, 2))
-b = Tensor(shape=(16, 1))
 
-res = w.matmul(X.T())
-#print(res)
+X = Tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
+W = Tensor(fill_random=True, shape=(10, 3))
+b = Tensor(shape=(10, 1))
 
+Xt = X.T
+mul = W.dot(Xt)
+res = mul + b
 
-res1 = w + b
-res1.grad = np.ones(res1.shape())
-# print(res1.grad)
-res1._backward()
-#print(res1)
-print(w.grad)
+res.grad = np.ones(res.shape)
+res.grad_func()
+mul.grad_func()
 print(b.grad)
